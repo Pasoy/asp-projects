40375599a03dd3738654fb0716914b111e3e554b
Pasoy <pascal34287@gmail.com>
2020-05-21 15:05:30 +0200

    validation for forms

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -6,11 +6,23 @@ using System.Web;
 
 namespace Vidly.Models
 {
+    /*
+     * Other Data Annotations
+     * [Required]
+     * [StringLength(255)]
+     * [Range(1, 10)]
+     * [Compare("OtherProperty")]
+     * [Phone]
+     * [EmailAddress]
+     * [Url]
+     * [RegularExpression("..")]
+     * ...
+     */
     public class Customer
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a customer's name!")]
         [StringLength(255)]
         public string Name { get; set; }
 
@@ -23,6 +35,7 @@ namespace Vidly.Models
 
         //nullable birthday - not required
         [Display(Name = "Date of Birth")]
+        [Min18Years]
         public DateTime? Birthdate { get; set; }
     }
 }
