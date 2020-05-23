92237882eeeaa869435b9387e24e74ddd903bc40
Pasoy <pascal34287@gmail.com>
2020-05-23 15:09:58 +0200

    camel notation api, ihttpactionresult

--- Vidly/Vidly/DTOs/CustomerDto.cs
+++ Vidly/Vidly/DTOs/CustomerDto.cs
@@ -11,6 +11,8 @@ namespace Vidly.DTOs
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
